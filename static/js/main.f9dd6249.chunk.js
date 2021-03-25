bfd578ba6b9640e36023f92127484359f4f428ea
Dema Maksim <maksimdema@gmail.com>
2021-03-25 13:45:12 +0200

    Updates

---
+++ static/js/main.f9dd6249.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{9:function(e,o,t){"use strict";t.r(o);var n=t(1),c=t.n(n),r=t(3),h=t.n(r),i=t(0),s=function(){return Object(i.jsx)("div",{children:"Hello hw_02 phonebook"})};h.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.f9dd6249.chunk.js.map
